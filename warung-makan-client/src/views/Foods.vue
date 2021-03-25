3fb3d6999c58ea6dd3fc5d9e51b4f8d481a9fef2
Yuda <yuda10aditya@gmail.com>
2021-03-25 18:44:34 +0700

    create Home Page

---
+++ warung-makan-client/src/views/Foods.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <Navbar />
+        <h2>Daftar Makanan</h2>
+    </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+    name: 'Foods',
+    components: {
+        Navbar
+        }
+
+}
+</script>
+
+<style>
+
+</style>
